b2422fd122ec5818ad1e37f0a9815e82e66806f6
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-30 11:42:09 +0100

    Initial Commit

---
+++ HratkySeSwiftem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HratkySeSwiftem
+//
+//  Created by Lukáš Stankovič on 30.01.16.
+//  Copyright © 2016 Lukáš Stankovič. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
