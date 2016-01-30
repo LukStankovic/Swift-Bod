53786026141da824cc018878f91176744907f80a
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-30 12:06:09 +0100

    Vytvoření třídy Bod

--- HratkySeSwiftem/main.swift
+++ HratkySeSwiftem/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+class Bod {
+    var x: Int = 0
+    var y: Int = 0
+    var popis: String = "Třída pro práci s body"
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+var a = Bod(x: 1, y: 4)
+var b = Bod(x: 3, y: 6)
+
 
